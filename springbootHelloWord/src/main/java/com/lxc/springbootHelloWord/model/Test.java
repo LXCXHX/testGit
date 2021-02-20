3924b72737af59fc5e5c4f3e0868f13c1db7181e
LXCXHX <18363065531@163.com>
2021-02-20 17:46:49 +0800

    aaaa

--- springbootHelloWord/src/main/java/com/lxc/springbootHelloWord/model/Test.java
+++ springbootHelloWord/src/main/java/com/lxc/springbootHelloWord/model/Test.java
@@ -3,6 +3,6 @@ package com.lxc.springbootHelloWord.model;
 public class Test {
     public static void main(String[] args) {
 		Student stu = new Student();
-		stu.getSex();
+		//stu.getSex();
 	}
 }
